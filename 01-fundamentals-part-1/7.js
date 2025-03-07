8c947236a18fbee03f56ea5f9b7e6c9aa488b042
me-anshu7 <me.anshu7@gmail.com>
2025-03-07 12:21:14 +0000

    initial commit

---
+++ 01-fundamentals-part-1/7.js
@@ -0,0 +1,8 @@
+/**
+ * => What is JavaScript?
+ * Javascript is a high-level, object-oriented, multi-paradigm programming language.
+ * programming language - is a tool that let's us write code that will instruct computer to do things 
+ * high-level - we dont have to worry about complex stuff like memory management, has lot of abstractions
+ * object-oriented - based on objects, for storing most kind of data
+ * multi-paradigm - js is flexible and versitle, we can basically use different programming styles such as declaritive and imperative
+ */
